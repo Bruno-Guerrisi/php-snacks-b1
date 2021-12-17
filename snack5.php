647215820c44bb5c57a4ea67609c7ef2f289d292
Bruno-Guerrisi <brunoguerrisi1973@outlook.it>
2021-12-17 18:21:07 +0100

    snack 5

---
+++ snack5.php
@@ -0,0 +1,19 @@
+<?php
+
+/* Snack 5
+Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo. */
+
+
+    $paragraph = 'Rocco è andato a fare la speso. Il negozio era chiuso. Rocco è tornato a casa triste';
+
+    $arrParagraph = explode('.', $paragraph );
+
+    echo 'Frase iniziale' .$arrParagraph[$i];
+    echo '<br>';
+
+    for ($i=0; $i < count($arrParagraph) ; $i++) { 
+       
+        echo 'Frase ' .($i + 1) .' : ' .$arrParagraph[$i];
+        echo '<br>';
+    }
+?>
